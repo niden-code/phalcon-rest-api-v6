6daa8e2c0c0b6cf8193e89e787fc74b492b58682
Nikolaos Dimopoulos <nikos@niden.net>
2025-08-26 18:25:00 -0500

    phpcs
    Refs #4

--- phpunit.php
+++ phpunit.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 # This file is part of Phalcon.
 #
 # (c) Phalcon Team <team@phalcon.io>
@@ -7,7 +9,6 @@
 # For the full copyright and license information, please view
 # the LICENSE file that was distributed with this source code.
 
-declare(strict_types=1);
 
 ini_set('xdebug.mode', 'coverage');
 
