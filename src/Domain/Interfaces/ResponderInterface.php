a01332327a59965c03152e6fb17f1e08c7439ffc
Nikolaos Dimopoulos <nikos@niden.net>
2025-09-13 14:40:40 -0500

    [#.x] - added interfaces for ADR

---
+++ src/Domain/Interfaces/ResponderInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of the Phalcon API.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Api\Domain\Interfaces;
+
+use Phalcon\Domain\Payload;
+use Phalcon\Http\ResponseInterface;
+
+interface ResponderInterface
+{
+    public function __invoke(Payload $payload): ResponseInterface;
+}
