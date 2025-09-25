f7f18a7b51f769b491a31a6e68f31a83e73f6795
Nikolaos Dimopoulos <nikos@niden.net>
2025-09-25 06:04:36 -0500

    [#.x] - started using providers, moved routing out

--- public/index.php
+++ public/index.php
@@ -2,64 +2,40 @@
 
 declare(strict_types=1);
 
-use Phalcon\Api\Domain\Interfaces\DomainInterface;
-use Phalcon\Api\Domain\Interfaces\ResponderInterface;
-use Phalcon\Api\Domain\Middleware\ResponseSender;
-use Phalcon\Api\Domain\Services\ActionHandler;
 use Phalcon\Api\Domain\Services\Container;
+use Phalcon\Api\Domain\Services\Providers\ErrorHandlerProvider;
+use Phalcon\Api\Domain\Services\Providers\RouterProvider;
+use Phalcon\Di\ServiceProviderInterface;
 use Phalcon\Mvc\Micro;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 $container   = new Container();
 $application = new Micro($container);
+$container->set(Container::APPLICATION, $application, true);
+$now = hrtime(true);
+$container->set(
+    Container::TIME,
+    function () use ($now) {
+        return $now;
+    },
+    true
+);
 
 /**
- * Routes
+ * Providers
  */
-$routes = [
-    [
-        'method'    => 'get',
-        'pattern'   => '/',
-        'service'   => Container::HELLO_SERVICE,
-        'responder' => Container::HELLO_RESPONDER_JSON,
-    ],
+$providers = [
+    ErrorHandlerProvider::class,
+    RouterProvider::class,
 ];
 
-foreach ($routes as $route) {
-    $method        = $route['method'];
-    $pattern       = $route['pattern'];
-    $serviceName   = $route['service'];
-    $responderName = $route['responder'];
-
-    $application->$method(
-        $pattern,
-        function () use ($container, $serviceName, $responderName) {
-            /** @var DomainInterface $service */
-            $service   = $container->get($serviceName);
-            /** @var ResponderInterface $responder */
-            $responder = $container->get($responderName);
-
-            $action = new ActionHandler($service, $responder);
-            $action->__invoke();
-        }
-    );
+/** @var class-string $provider */
+foreach ($providers as $provider) {
+    /** @var ServiceProviderInterface $service */
+    $service = new $provider();
+    $container->register($service);
 }
-
-$application->finish(
-    function () use ($container) {
-        $response = $container->getShared(Container::RESPONSE);
-        $sender   = new ResponseSender();
-
-        $sender->__invoke($response);
-    }
-);
-
-$application->notFound(
-    function () {
-        echo "404 - Not Found - " . date("Y-m-d H:i:s");
-    }
-);
 
 
 /** @var string $uri */
