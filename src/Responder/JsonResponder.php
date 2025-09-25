e0e850b5da50678fa825a94aac3c04da8c69e3f3
Nikolaos Dimopoulos <nikos@niden.net>
2025-09-25 06:02:32 -0500

    [#.x] - adjustments to use new response class

--- src/Responder/JsonResponder.php
+++ src/Responder/JsonResponder.php
@@ -13,58 +13,27 @@ declare(strict_types=1);
 
 namespace Phalcon\Api\Responder;
 
-use DateTimeImmutable;
-use DateTimeZone;
 use Phalcon\Api\Domain\Interfaces\ResponderInterface;
+use Phalcon\Api\Domain\Services\Http\Response;
 use Phalcon\Domain\Payload;
-use Phalcon\Http\ResponseInterface;
-
-use function json_encode;
 
 final class JsonResponder implements ResponderInterface
 {
     public function __construct(
-        private ResponseInterface $response
+        private Response $response
     ) {
     }
 
-    public function __invoke(Payload $payload): ResponseInterface
+    public function __invoke(Payload $payload): Response
     {
-        $result  = $payload->getResult();
+        $result = $payload->getResult();
         /** @var string $content */
         $content = $result['results'];
 
-        $timestamp = new DateTimeImmutable('now', new DateTimeZone('UTC'));
-        $dateTime  = $timestamp->format('Y-m-d H:i:s');
-        $output = [
-            'data'   => [
-                $content
-            ],
-            'errors' => [],
-            'meta'   => [
-                'code'      => 200,
-                'hash'      => '',
-                'message'   => 'success',
-                'timestamp' => $dateTime,
-            ]
-        ];
-
-        $dataErrors = [
-            'data'   => $output['data'],
-            'errors' => $output['errors'],
-        ];
-        $encoded  = json_encode($dataErrors);
-        $encoded  = (false === $encoded) ? '' : $encoded;
-        $hash     = sha1($dateTime . $encoded);
-        $eTag     = sha1($encoded);
-
-        $output['meta']['hash'] = $hash;
-
         $this
             ->response
-            ->setContentType('application/json')
-            ->setHeader('E-Tag', $eTag)
-            ->setJsonContent($output)
+            ->withPayloadData([$content])
+            ->render()
         ;
 
         return $this->response;
