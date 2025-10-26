87ca7d98f84fa34ade23bf4e1ef2e1255be170f3
Nikolaos Dimopoulos <nikos@niden.net>
2025-10-25 21:57:13 -0500

    [#.x] - added new services (cache, jwt etc.)

--- src/Domain/Components/Container.php
+++ src/Domain/Components/Container.php
@@ -13,15 +13,22 @@ declare(strict_types=1);
 
 namespace Phalcon\Api\Domain\Components;
 
+use Phalcon\Api\Domain\Components\Cache\Cache;
 use Phalcon\Api\Domain\Components\DataSource\QueryRepository;
 use Phalcon\Api\Domain\Components\DataSource\TransportRepository;
+use Phalcon\Api\Domain\Components\Encryption\JWTToken;
 use Phalcon\Api\Domain\Components\Encryption\Security;
 use Phalcon\Api\Domain\Components\Env\EnvManager;
 use Phalcon\Api\Domain\Components\Middleware\HealthMiddleware;
 use Phalcon\Api\Domain\Components\Middleware\NotFoundMiddleware;
-use Phalcon\Api\Domain\Services\Hello\HelloService;
+use Phalcon\Api\Domain\Components\Middleware\ValidateTokenClaimsMiddleware;
+use Phalcon\Api\Domain\Components\Middleware\ValidateTokenPresenceMiddleware;
+use Phalcon\Api\Domain\Components\Middleware\ValidateTokenStructureMiddleware;
+use Phalcon\Api\Domain\Components\Middleware\ValidateTokenUserMiddleware;
+use Phalcon\Api\Domain\Services\Auth\LoginPostService;
 use Phalcon\Api\Domain\Services\User\UserGetService;
 use Phalcon\Api\Responder\JsonResponder;
+use Phalcon\Cache\AdapterFactory;
 use Phalcon\DataMapper\Pdo\Connection;
 use Phalcon\Di\Di;
 use Phalcon\Di\Service;
@@ -32,30 +39,39 @@ use Phalcon\Http\Response;
 use Phalcon\Logger\Adapter\Stream;
 use Phalcon\Logger\Logger;
 use Phalcon\Mvc\Router;
+use Phalcon\Storage\SerializerFactory;
 
 class Container extends Di
 {
     /** @var string */
     public const APPLICATION = 'application';
+    /**
+     * Services
+     */
+    public const AUTH_LOGIN_POST_SERVICE = 'service.auth.login.post';
     /** @var string */
     public const CACHE = 'cache';
     /** @var string */
     public const CONNECTION = 'connection';
     /** @var string */
+    public const ENV = 'env';
+    /** @var string */
     public const EVENTS_MANAGER = 'eventsManager';
     /** @var string */
     public const FILTER = 'filter';
-    /**
-     * Services
-     */
-    public const HELLO_SERVICE = HelloService::class;
+    /** @var string */
+    public const JWT_TOKEN = 'jwt.token';
     /** @var string */
     public const LOGGER = 'logger';
     /**
      * Middleware
      */
-    public const MIDDLEWARE_HEALTH    = HealthMiddleware::class;
-    public const MIDDLEWARE_NOT_FOUND = NotFoundMiddleware::class;
+    public const MIDDLEWARE_HEALTH                  = HealthMiddleware::class;
+    public const MIDDLEWARE_NOT_FOUND               = NotFoundMiddleware::class;
+    public const MIDDLEWARE_VALIDATE_TOKEN_CLAIMS   = ValidateTokenClaimsMiddleware::class;
+    public const MIDDLEWARE_VALIDATE_TOKEN_PRESENCE = ValidateTokenPresenceMiddleware::class;
+    public const MIDDLEWARE_VALIDATE_TOKEN_STRUCTURE = ValidateTokenStructureMiddleware::class;
+    public const MIDDLEWARE_VALIDATE_TOKEN_USER      = ValidateTokenUserMiddleware::class;
     /**
      * Repositories
      */
@@ -67,6 +83,7 @@ class Container extends Di
      * Responders
      */
     public const RESPONDER_JSON = JsonResponder::class;
+//    public const MIDDLEWARE_VALIDATE_TOKEN_REVOKED   = ValidateTokenRevokedMiddleware::class;
     /** @var string */
     public const RESPONSE = 'response';
     /** @var string */
@@ -74,25 +91,115 @@ class Container extends Di
     /** @var string */
     public const SECURITY = Security::class;
     /** @var string */
-    public const TIME             = 'time';
-    public const USER_GET_SERVICE = 'service.user.get';
+    public const TIME = 'time';
+    public const USER_GET_SERVICE        = 'service.user.get';
 
     public function __construct()
     {
         $this->services = [
+            self::CACHE          => $this->getServiceCache(),
             self::CONNECTION     => $this->getServiceConnection(),
+            self::ENV            => $this->getServiceEnv(),
             self::EVENTS_MANAGER => $this->getServiceEventsManger(),
             self::FILTER         => $this->getServiceFilter(),
+            self::JWT_TOKEN      => $this->getServiceJWTToken(),
             self::LOGGER         => $this->getServiceLogger(),
             self::REQUEST        => new Service(Request::class, true),
             self::RESPONSE       => new Service(Response::class, true),
             self::ROUTER         => $this->getServiceRouter(),
 
-            self::USER_GET_SERVICE => $this->getServiceUserGet(),
-            self::REPOSITORY       => $this->getServiceRepository(),
+            self::REPOSITORY => $this->getServiceRepository(),
+
+            self::AUTH_LOGIN_POST_SERVICE => $this->getServiceAuthLoginPost(),
+            self::USER_GET_SERVICE        => $this->getServiceUserGet(),
         ];
 
         parent::__construct();
+    }
+
+    /**
+     * @return Service
+     */
+    private function getServiceAuthLoginPost(): Service
+    {
+        return new Service(
+            [
+                'className' => LoginPostService::class,
+                'arguments' => [
+                    [
+                        'type' => 'service',
+                        'name' => self::REPOSITORY,
+                    ],
+                    [
+                        'type' => 'service',
+                        'name' => self::REPOSITORY_TRANSPORT,
+                    ],
+                    [
+                        'type' => 'service',
+                        'name' => self::CACHE,
+                    ],
+                    [
+                        'type' => 'service',
+                        'name' => self::ENV,
+                    ],
+                    [
+                        'type' => 'service',
+                        'name' => self::JWT_TOKEN,
+                    ],
+                    [
+                        'type' => 'service',
+                        'name' => self::FILTER,
+                    ],
+                    [
+                        'type' => 'service',
+                        'name' => self::SECURITY,
+                    ],
+                ],
+            ]
+        );
+    }
+
+    /**
+     * @return Service
+     */
+    private function getServiceCache(): Service
+    {
+        return new Service(
+            function () {
+                /** @var EnvManager $env */
+                $env = $this->getShared(self::ENV);
+
+                /** @var string $prefix */
+                $prefix = $env->get('CACHE_PREFIX', '-rest-');
+                /** @var string $host */
+                $host = $env->get('CACHE_HOST', 'localhost');
+                /** @var int $lifetime */
+                $lifetime = $env->get('CACHE_LIFETIME', Cache::CACHE_LIFETIME_DAY, 'int');
+                /** @var int $index */
+                $index = $env->get('CACHE_INDEX', 0, 'int');
+                /** @var int $port */
+                $port = $env->get('CACHE_PORT', 6379, 'int');
+
+                $options = [
+                    'host'     => $host,
+                    'index'    => $index,
+                    'lifetime' => $lifetime,
+                    'prefix'   => $prefix,
+                    'port'     => $port,
+                    'uniqueId' => $prefix,
+                ];
+
+                /** @var string $adapter */
+                $adapter = $env->get('CACHE_ADAPTER', 'redis');
+
+                $serializerFactory = new SerializerFactory();
+                $adapterFactory    = new AdapterFactory($serializerFactory);
+                $cacheAdapter      = $adapterFactory->newInstance($adapter, $options);
+
+                return new Cache($cacheAdapter);
+            },
+            true
+        );
     }
 
     /**
@@ -102,17 +209,21 @@ class Container extends Di
     {
         return new Service(
             function () {
+                /** @var EnvManager $env */
+                $env = $this->getShared(self::ENV);
+
                 /** @var string $dbName */
-                $dbName = EnvManager::get('DB_NAME', 'phalcon');
+                $dbName = $env->get('DB_NAME', 'phalcon');
                 /** @var string $host */
-                $host = EnvManager::get('DB_HOST', 'rest-db');
+                $host = $env->get('DB_HOST', 'rest-db');
                 /** @var string $password */
-                $password = EnvManager::get('DB_PASS', 'secret');
-                $port     = (int)EnvManager::get('DB_PORT', 3306);
+                $password = $env->get('DB_PASS', 'secret');
+                /** @var int $port */
+                $port = $env->get('DB_PORT', 3306, 'int');
                 /** @var string $username */
-                $username = EnvManager::get('DB_USER', 'root');
+                $username = $env->get('DB_USER', 'root');
                 /** @var string $encoding */
-                $encoding = EnvManager::get('DB_CHARSET', 'utf8');
+                $encoding = $env->get('DB_CHARSET', 'utf8');
                 $queries  = ['SET NAMES utf8mb4'];
                 $dsn      = sprintf(
                     'mysql:host=%s;port=%s;dbname=%s;charset=%s',
@@ -129,6 +240,19 @@ class Container extends Di
                     [],
                     $queries
                 );
+            },
+            true
+        );
+    }
+
+    /**
+     * @return Service
+     */
+    private function getServiceEnv(): Service
+    {
+        return new Service(
+            function () {
+                return new EnvManager();
             },
             true
         );
@@ -166,16 +290,37 @@ class Container extends Di
     /**
      * @return Service
      */
+    private function getServiceJWTToken(): Service
+    {
+        return new Service(
+            [
+                'className' => JWTToken::class,
+                'arguments' => [
+                    [
+                        'type' => 'service',
+                        'name' => self::ENV,
+                    ],
+                ],
+            ]
+        );
+    }
+
+    /**
+     * @return Service
+     */
     private function getServiceLogger(): Service
     {
-        /** @var string $logName */
-        $logName = EnvManager::get('LOG_FILENAME', 'rest-api');
-        /** @var string $logPath */
-        $logPath = EnvManager::get('LOG_PATH', 'storage/logs/');
-        $logFile = EnvManager::appPath($logPath) . '/' . $logName . '.log';
-
         return new Service(
-            function () use ($logName, $logFile) {
+            function () {
+                /** @var EnvManager $env */
+                $env = $this->getShared(self::ENV);
+
+                /** @var string $logName */
+                $logName = $env->get('LOG_FILENAME', 'rest-api');
+                /** @var string $logPath */
+                $logPath = $env->get('LOG_PATH', 'storage/logs/');
+                $logFile = $env->appPath($logPath) . '/' . $logName . '.log';
+
                 return new Logger(
                     $logName,
                     [
