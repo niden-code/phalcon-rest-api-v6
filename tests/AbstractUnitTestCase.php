10fa5bfd1fe58243c2fd1ccad3dd6bf96875000b
Nikolaos Dimopoulos <nikos@niden.net>
2025-10-25 21:57:40 -0500

    [#.x] - added getToken and strengthened password generation

--- tests/AbstractUnitTestCase.php
+++ tests/AbstractUnitTestCase.php
@@ -13,13 +13,17 @@ declare(strict_types=1);
 
 namespace Phalcon\Api\Tests;
 
+use DateTimeImmutable;
 use Faker\Factory;
 use PDO;
 use Phalcon\Api\Domain\Components\Constants\Dates;
 use Phalcon\Api\Domain\Components\Container;
 use Phalcon\Api\Domain\Components\Encryption\Security;
+use Phalcon\Api\Domain\Components\Enums\Common\JWTEnum;
 use Phalcon\Api\Tests\Fixtures\Domain\Migrations\UsersMigration;
 use Phalcon\DataMapper\Pdo\Connection;
+use Phalcon\Encryption\Security\JWT\Builder;
+use Phalcon\Encryption\Security\JWT\Signer\Hmac;
 use PHPUnit\Framework\TestCase;
 
 abstract class AbstractUnitTestCase extends TestCase
@@ -123,7 +127,7 @@ abstract class AbstractUnitTestCase extends TestCase
         return [
             'usr_id'             => 0,
             'usr_status_flag'    => $fields['usr_status_flag'] ?? 1,
-            'usr_email'          => $fields['usr_email'] ?? $faker->email,
+            'usr_email'          => $fields['usr_email'] ?? $faker->email(),
             'usr_password'       => $password,
             'usr_name_prefix'    => $fields['usr_name_prefix'] ?? $faker->title(),
             'usr_name_first'     => $fields['usr_name_first'] ?? $faker->firstName(),
@@ -148,7 +152,59 @@ abstract class AbstractUnitTestCase extends TestCase
      */
     public function getStrongPassword(): string
     {
-        return substr(base64_encode(random_bytes(512)), 0, 128);
+        $password = base64_encode(random_bytes(128));
+        $special  = '!@#$%^&*()-_=+[]{}|;:,.<>?/~`';
+
+        $passwordArray = str_split($password);
+        $specialArray  = str_split($special);
+
+        $result = array_merge($passwordArray, $specialArray);
+        shuffle($result);
+
+        $password = implode('', $result);
+
+        return substr($password, 0, 128);
+    }
+
+    /**
+     * @return string
+     */
+    public function getUserToken(array $user): string
+    {
+        $audience = $user['audience'] ?? 'https://rest-api.phalcon.io';
+        $expires  = $user['expires'] ?? '+60 seconds';
+        $algo     = $user['algo'] ?? 'sha512';
+
+        $tokenBuilder = new Builder(new Hmac($algo));
+        /** @var non-empty-string $issuer */
+        $issuer = $user['usr_issuer'];
+        /** @var non-empty-string $password */
+        $password = $user['usr_token_password'];
+        /** @var non-empty-string $tokenId */
+        $tokenId = $user['usr_token_id'];
+        /** @var non-empty-string $userId */
+        $userId = $user['usr_id'];
+        $now    = new DateTimeImmutable();
+        /**
+         *  Just in case so that the token nbf and iss are correct
+         */
+        $now       = $now->modify('-2 seconds');
+        $expiresAt = $now->modify($expires);
+
+        $tokenObject = $tokenBuilder
+            ->setIssuer($issuer)
+            ->setAudience($audience)
+            ->setId($tokenId)
+            ->setNotBefore($now->getTimestamp())
+            ->setIssuedAt($now->getTimestamp())
+            ->setExpirationTime($expiresAt->getTimestamp())
+            ->setPassphrase($password)
+            ->addClaim(JWTEnum::UserId->value, $userId)
+            ->addClaim(JWTEnum::Refresh->value, false)
+            ->getToken()
+        ;
+
+        return $tokenObject->getToken();
     }
 
     public function setUp(): void
