dc5a13241d46a1dd549fa2f37b139d7292ec9813
Nikolaos Dimopoulos <nikos@niden.net>
2025-10-25 21:58:22 -0500

    [#.x] - new session transport properties

--- src/Domain/Components/DataSource/TransportRepository.php
+++ src/Domain/Components/DataSource/TransportRepository.php
@@ -15,12 +15,62 @@ namespace Phalcon\Api\Domain\Components\DataSource;
 
 use Phalcon\Api\Domain\Components\DataSource\User\UserTransport;
 use Phalcon\Api\Domain\Components\DataSource\User\UserTypes;
+use Phalcon\Encryption\Security\JWT\Token\Token;
 
 /**
+ * @phpstan-import-type TLoginResponsePayload from UserTypes
+ * @phpstan-import-type TUserDbRecord from UserTypes
  * @phpstan-import-type TUserRecord from UserTypes
  */
-final readonly class TransportRepository
+final class TransportRepository
 {
+    private ?Token $sessionToken = null;
+
+    private ?UserTransport $sessionUser = null;
+
+    /**
+     * Returns the session token.
+     *
+     * @return Token|null
+     */
+    public function getSessionToken(): ?Token
+    {
+        return $this->sessionToken;
+    }
+
+    /**
+     * Returns the session user.
+     *
+     * @return UserTransport|null
+     */
+    public function getSessionUser(): ?UserTransport
+    {
+        return $this->sessionUser;
+    }
+
+    /**
+     * @param UserTransport $user
+     * @param string        $token
+     *
+     * @return TLoginResponsePayload
+     */
+    public function newLoginUser(
+        UserTransport $user,
+        string $token
+    ): array {
+        return [
+            'authenticated' => true,
+            'user'          => [
+                'id'    => $user->getId(),
+                'name'  => $user->getFullName(),
+                'email' => $user->getEmail(),
+            ],
+            'jwt'           => [
+                'token' => $token,
+            ],
+        ];
+    }
+
     /**
      * @param TUserRecord $dbUser
      *
@@ -29,5 +79,29 @@ final readonly class TransportRepository
     public function newUser(array $dbUser): UserTransport
     {
         return new UserTransport($dbUser);
+    }
+
+    /**
+     * Sets the session Token
+     *
+     * @param Token $token
+     *
+     * @return void
+     */
+    public function setSessionToken(Token $token): void
+    {
+        $this->sessionToken = $token;
+    }
+
+    /**
+     * Populates the session user with the user data.
+     *
+     * @param TUserDbRecord $user
+     *
+     * @return void
+     */
+    public function setSessionUser(array $user): void
+    {
+        $this->sessionUser = $this->newUser($user);
     }
 }
