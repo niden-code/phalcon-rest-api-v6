0a5e19cf6bab427876b53761862532abad7b3721
Nikolaos Dimopoulos <nikos@niden.net>
2025-11-05 11:52:09 -0600

    [#.x] - added user delete service

---
+++ src/Domain/Services/User/UserDeleteService.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * This file is part of the Phalcon API.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Api\Domain\Services\User;
+
+use PayloadInterop\DomainStatus;
+use Phalcon\Api\Domain\ADR\InputTypes;
+use Phalcon\Domain\Payload;
+
+/**
+ * @phpstan-import-type TUserInput from InputTypes
+ */
+final class UserDeleteService extends AbstractUserService
+{
+    /**
+     * @param TUserInput $input
+     *
+     * @return Payload
+     */
+    public function __invoke(array $input): Payload
+    {
+        $userId = $this->filter->absint($input['id'] ?? 0);
+
+        /**
+         * Success
+         */
+        if ($userId > 0) {
+            $rows = $this->repository->user()->deleteById($userId);
+
+            if ($rows > 0) {
+                return new Payload(
+                    DomainStatus::DELETED,
+                    [
+                        'data' => [
+                            'Record deleted successfully [#' . $userId . '].',
+                        ],
+                    ]
+                );
+            }
+        }
+
+        /**
+         * 404
+         */
+        return new Payload(
+            DomainStatus::NOT_FOUND,
+            [
+                'errors' => [
+                    'Record(s) not found',
+                ],
+            ]
+        );
+    }
+}
