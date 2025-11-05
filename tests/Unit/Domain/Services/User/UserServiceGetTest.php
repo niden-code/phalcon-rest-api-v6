309784a17d09be06fbe89a36238c1be383f4a81b
Nikolaos Dimopoulos <nikos@niden.net>
2025-11-05 11:53:02 -0600

    [#.x] - reworking tests - moving dispatch tests to own file

--- tests/Unit/Domain/Services/User/UserServiceGetTest.php
+++ tests/Unit/Domain/Services/User/UserServiceGetTest.php
@@ -15,64 +15,14 @@ namespace Phalcon\Api\Tests\Unit\Domain\Services\User;
 
 use PayloadInterop\DomainStatus;
 use Phalcon\Api\Domain\Components\Container;
-use Phalcon\Api\Domain\Components\DataSource\TransportRepository;
-use Phalcon\Api\Domain\Components\Env\EnvManager;
 use Phalcon\Api\Domain\Services\User\UserGetService;
 use Phalcon\Api\Tests\AbstractUnitTestCase;
 use Phalcon\Api\Tests\Fixtures\Domain\Migrations\UsersMigration;
 use PHPUnit\Framework\Attributes\BackupGlobals;
 
 #[BackupGlobals(true)]
-final class UserServiceTest extends AbstractUnitTestCase
+final class UserServiceGetTest extends AbstractUnitTestCase
 {
-    public function testDispatch(): void
-    {
-        /** @var EnvManager $env */
-        $env = $this->container->getShared(Container::ENV);
-        /** @var TransportRepository $transport */
-        $transport = $this->container->get(Container::REPOSITORY_TRANSPORT);
-
-        $migration = new UsersMigration($this->getConnection());
-        $dbUser    = $this->getNewUser($migration);
-        $userId    = $dbUser['usr_id'];
-        $token     = $this->getUserToken($dbUser);
-
-        $time    = $_SERVER['REQUEST_TIME_FLOAT'] ?? time();
-        $_SERVER = [
-            'REQUEST_METHOD'     => 'GET',
-            'REQUEST_TIME_FLOAT' => $time,
-            'HTTP_AUTHORIZATION' => 'Bearer ' . $token,
-            'REQUEST_URI'        => '/user',
-        ];
-
-        $_GET = [
-            'userId' => $userId,
-        ];
-
-        ob_start();
-        require_once $env->appPath('public/index.php');
-        $response = ob_get_clean();
-
-        $contents = json_decode($response, true);
-
-        restore_error_handler();
-
-        $this->assertArrayHasKey('data', $contents);
-        $this->assertArrayHasKey('errors', $contents);
-
-        $data   = $contents['data'];
-        $errors = $contents['errors'];
-
-        $expected = [];
-        $actual   = $errors;
-        $this->assertSame($expected, $actual);
-
-        $user     = $transport->newUser($dbUser);
-        $expected = $user->toArray();
-        $actual   = $data;
-        $this->assertSame($expected, $actual);
-    }
-
     public function testServiceEmptyUserId(): void
     {
         /** @var UserGetService $service */
@@ -103,7 +53,7 @@ final class UserServiceTest extends AbstractUnitTestCase
 
         $payload = $service->__invoke(
             [
-                'userId' => $userId,
+                'id' => $userId,
             ]
         );
 
@@ -194,7 +144,7 @@ final class UserServiceTest extends AbstractUnitTestCase
 
         $payload = $service->__invoke(
             [
-                'userId' => 999999,
+                'id' => 999999,
             ]
         );
 
