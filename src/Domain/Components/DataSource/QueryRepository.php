a19d744449c31b741569a31f91df1d3f139725c5
Nikolaos Dimopoulos <nikos@niden.net>
2025-11-07 11:46:17 -0600

    [#.x] - switched to interfaces

--- src/Domain/Components/DataSource/QueryRepository.php
+++ src/Domain/Components/DataSource/QueryRepository.php
@@ -13,28 +13,31 @@ declare(strict_types=1);
 
 namespace Phalcon\Api\Domain\Components\DataSource;
 
+use Phalcon\Api\Domain\Components\DataSource\User\UserMapper;
 use Phalcon\Api\Domain\Components\DataSource\User\UserRepository;
+use Phalcon\Api\Domain\Components\DataSource\User\UserRepositoryInterface;
 use Phalcon\DataMapper\Pdo\Connection;
 
 class QueryRepository
 {
-    private ?UserRepository $user = null;
+    private ?UserRepositoryInterface $user = null;
 
     /**
      * @param Connection $connection
      */
     public function __construct(
         private readonly Connection $connection,
+        private readonly UserMapper $userMapper,
     ) {
     }
 
     /**
      * @return UserRepository
      */
-    public function user(): UserRepository
+    public function user(): UserRepositoryInterface
     {
         if (null === $this->user) {
-            $this->user = new UserRepository($this->connection);
+            $this->user = new UserRepository($this->connection, $this->userMapper);
         }
 
         return $this->user;
