baa077d0d357743a9469b7e10d4e1ddeeb990059
Nikolaos Dimopoulos <nikos@niden.net>
2025-11-07 11:42:43 -0600

    [#.x] - adjusted for new dependencies

--- src/Domain/Services/User/AbstractUserService.php
+++ src/Domain/Services/User/AbstractUserService.php
@@ -15,17 +15,62 @@ namespace Phalcon\Api\Domain\Services\User;
 
 use Phalcon\Api\Domain\ADR\DomainInterface;
 use Phalcon\Api\Domain\Components\DataSource\QueryRepository;
-use Phalcon\Api\Domain\Components\DataSource\TransportRepository;
+use Phalcon\Api\Domain\Components\DataSource\SanitizerInterface;
+use Phalcon\Api\Domain\Components\DataSource\User\User;
+use Phalcon\Api\Domain\Components\DataSource\User\UserInput;
+use Phalcon\Api\Domain\Components\DataSource\User\UserMapper;
+use Phalcon\Api\Domain\Components\DataSource\User\UserValidator;
 use Phalcon\Api\Domain\Components\Encryption\Security;
-use Phalcon\Filter\Filter;
+use Phalcon\Api\Domain\Components\Enums\Http\HttpCodesEnum;
+use Phalcon\Api\Domain\Components\Payload;
+
+use function array_shift;
 
 abstract class AbstractUserService implements DomainInterface
 {
+    protected HttpCodesEnum $errorMessage;
+
     public function __construct(
         protected readonly QueryRepository $repository,
-        protected readonly TransportRepository $transport,
-        protected readonly Filter $filter,
+        protected readonly UserMapper $mapper,
+        protected readonly UserValidator $validator,
+        protected readonly SanitizerInterface $sanitizer,
         protected readonly Security $security,
+
     ) {
+    }
+
+    /**
+     * @param string $message
+     *
+     * @return Payload
+     */
+    protected function getErrorPayload(string $message): Payload
+    {
+        return Payload::error(
+            [
+                $this->errorMessage->text() . $message,
+            ]
+        );
+    }
+
+    /**
+     * @param UserInput $inputObject
+     *
+     * @return array
+     */
+    protected function processPassword(UserInput $inputObject): User
+    {
+        $inputData = $inputObject->toArray();
+        $inputData = array_shift($inputData);
+
+        if (null !== $inputData['password']) {
+            $plain  = $inputData['password'];
+            $hashed = $this->security->hash($plain);
+
+            $inputData['password'] = $hashed;
+        }
+
+        return $this->mapper->input($inputData);
     }
 }
