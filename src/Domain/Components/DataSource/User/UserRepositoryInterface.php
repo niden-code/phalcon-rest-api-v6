e117370b8304f05d5b2aabb2b9b625172048723e
Nikolaos Dimopoulos <nikos@niden.net>
2025-11-07 11:41:34 -0600

    [#.x] - added interface for user repository

---
+++ src/Domain/Components/DataSource/User/UserRepositoryInterface.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * This file is part of the Phalcon API.
+ *
+ * (c) Phalcon Team <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Phalcon\Api\Domain\Components\DataSource\User;
+
+/**
+ * @phpstan-import-type TUserRecord from UserTypes
+ * @phpstan-import-type TUserInsert from UserTypes
+ * @phpstan-import-type TUserUpdate from UserTypes
+ */
+interface UserRepositoryInterface
+{
+    /**
+     * @param array<string, bool|int|string|null> $criteria
+     *
+     * @return int
+     */
+    public function deleteBy(array $criteria): int;
+
+    /**
+     * @param int $recordId
+     *
+     * @return int
+     */
+    public function deleteById(int $recordId): int;
+
+    /**
+     * @param string $email
+     *
+     * @return User|null
+     */
+    public function findByEmail(string $email): ?User;
+
+    /**
+     * @param int $recordId
+     *
+     * @return User|null
+     */
+    public function findById(int $recordId): ?User;
+
+    /**
+     * @param array<string, bool|int|string|null> $criteria
+     *
+     * @return User|null
+     */
+    public function findOneBy(array $criteria): ?User;
+
+    /**
+     * @param User $user
+     *
+     * @return int
+     */
+    public function insert(User $user): int;
+
+    /**
+     * @param User $user
+     *
+     * @return int
+     */
+    public function update(User $user): int;
+}
